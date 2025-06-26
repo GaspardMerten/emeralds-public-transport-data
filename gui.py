e9a3e0f3ebe10417ecd1411f3a87a0df6202d3da
Gaspard Merten <gaspard.mp.work@gmail.com>
2025-06-26 16:46:28 +0200

    Trying to fex memory issue

--- gui.py
+++ gui.py
@@ -131,7 +131,7 @@ if feed:
                         data = fetch_data(start_date, end_date, feed_path=feed_path,
                                           parse_date=provider.get('file_to_period', None),
                                           timezone_str=provider.get('timezone', 'UTC'),
-                                          limit=10000
+                                          limit=100000
                                           )
 
                     if data:
